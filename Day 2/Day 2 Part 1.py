f8c87f9dac2f3a96ea5df1249c2ad989ef308ee7
Oliver Swindlehurst <50767261+oliverSwindlehurst@users.noreply.github.com>
2024-12-02 19:55:25 +0000

    Closest attempt i could make

--- Day 2/Day 2 Part 1.py
+++ Day 2/Day 2 Part 1.py
@@ -1,7 +1,22 @@
 with open("Day 2\InputTest.txt") as file_in:
     input = []
     for line in file_in:
-        input.append(line)
+        input.append([int(x) for x in line.split()])
+
+answer = 0
+counter = 0
 
 for row in input:
-    print(row)
+    numberToTest = row[0]
+    for number in row:
+        if(numberToTest > number and not(numberToTest >= number + 3)):
+            counter += 1
+        if(numberToTest < number and not(numberToTest <= number - 3)):
+            counter += 1
+        
+        if(counter == len(row) - 1):
+            answer += 1
+        numberToTest = number
+    counter = 0    
+
+print(answer)
